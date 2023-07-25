1748b0959ec0b4ea0ca94f9649fda34978ad2b6e
Daniele Alberti <kadmos@kadmos.com>
2023-07-25 10:21:19 +0200

    Added printPreviewDialog sample

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,9 +28,9 @@
         /// </summary>
         private void InitializeComponent()
         {
-            DXFReaderNET.DxfDocument dxfDocument1 = new DXFReaderNET.DxfDocument();
-            DXFReaderNET.AciColor aciColor1 = new DXFReaderNET.AciColor();
-            DXFReaderNET.Objects.RasterVariables rasterVariables1 = new DXFReaderNET.Objects.RasterVariables();
+            DXFReaderNET.DxfDocument dxfDocument2 = new DXFReaderNET.DxfDocument();
+            DXFReaderNET.AciColor aciColor2 = new DXFReaderNET.AciColor();
+            DXFReaderNET.Objects.RasterVariables rasterVariables2 = new DXFReaderNET.Objects.RasterVariables();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.dxfReaderNETControl1 = new DXFReaderNET.DXFReaderNETControl();
             this.button1 = new System.Windows.Forms.Button();
@@ -39,6 +39,8 @@
             this.printPreviewControl1 = new System.Windows.Forms.PrintPreviewControl();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.printPreviewDialog1 = new System.Windows.Forms.PrintPreviewDialog();
             this.SuspendLayout();
             // 
             // dxfReaderNETControl1
@@ -46,41 +48,42 @@
             this.dxfReaderNETControl1.AutoSize = true;
             this.dxfReaderNETControl1.ContinuousHighlight = false;
             this.dxfReaderNETControl1.CustomCursor = DXFReaderNET.CustomCursorType.None;
-            dxfDocument1.ActiveLayout = "Model";
-            dxfDocument1.Comments = null;
-            aciColor1.Index = ((short)(256));
-            aciColor1.IsByBlock = false;
-            aciColor1.IsByLayer = true;
-            aciColor1.UseTrueColor = false;
-            dxfDocument1.CurrentColor = aciColor1;
-            dxfDocument1.CurrentElevation = 0D;
-            dxfDocument1.CurrentLayer = "0";
-            dxfDocument1.CurrentLineTypeName = "ByLayer";
-            dxfDocument1.CurrentLineTypeScale = 1D;
-            dxfDocument1.CurrentTextSize = 2.5D;
-            dxfDocument1.CurrentTextStyle = "STANDARD";
-            dxfDocument1.CurrentThickness = 0D;
-            dxfDocument1.DateCreated = new System.DateTime(2020, 1, 31, 13, 17, 29, 352);
-            dxfDocument1.DateLastEdited = new System.DateTime(2020, 1, 31, 13, 17, 29, 352);
-            dxfDocument1.Handle = "0";
-            dxfDocument1.IsBinary = false;
-            dxfDocument1.Modified = false;
-            dxfDocument1.Name = null;
-            dxfDocument1.Owner = null;
-            rasterVariables1.DisplayFrame = true;
-            rasterVariables1.DisplayQuality = DXFReaderNET.Objects.ImageDisplayQuality.High;
-            rasterVariables1.Handle = "2D";
-            rasterVariables1.Owner = null;
-            rasterVariables1.Units = DXFReaderNET.ImageUnits.Unitless;
-            dxfDocument1.RasterVariables = rasterVariables1;
-            dxfDocument1.ThumbnailImage = null;
-            dxfDocument1.TotalEditTime = new System.DateTime(2020, 1, 31, 0, 0, 0, 0);
-            dxfDocument1.TotalEditTimeDays = 0;
-            dxfDocument1.UserTimer = new System.DateTime(2020, 1, 31, 0, 0, 0, 0);
-            dxfDocument1.UserTimerDays = 0;
-            this.dxfReaderNETControl1.DXF = dxfDocument1;
+            dxfDocument2.ActiveLayout = "Model";
+            dxfDocument2.Comments = null;
+            aciColor2.Index = ((short)(256));
+            aciColor2.IsByBlock = false;
+            aciColor2.IsByLayer = true;
+            aciColor2.UseTrueColor = false;
+            dxfDocument2.CurrentColor = aciColor2;
+            dxfDocument2.CurrentElevation = 0D;
+            dxfDocument2.CurrentLayer = "0";
+            dxfDocument2.CurrentLineTypeName = "ByLayer";
+            dxfDocument2.CurrentLineTypeScale = 1D;
+            dxfDocument2.CurrentTextSize = 2.5D;
+            dxfDocument2.CurrentTextStyle = "STANDARD";
+            dxfDocument2.CurrentThickness = 0D;
+            dxfDocument2.DateCreated = new System.DateTime(2020, 1, 31, 13, 17, 29, 352);
+            dxfDocument2.DateLastEdited = new System.DateTime(2020, 1, 31, 13, 17, 29, 352);
+            dxfDocument2.Handle = "0";
+            dxfDocument2.IsBinary = false;
+            dxfDocument2.Modified = false;
+            dxfDocument2.Name = null;
+            dxfDocument2.Owner = null;
+            rasterVariables2.DisplayFrame = true;
+            rasterVariables2.DisplayQuality = DXFReaderNET.Objects.ImageDisplayQuality.High;
+            rasterVariables2.Handle = "2D";
+            rasterVariables2.Owner = null;
+            rasterVariables2.Units = DXFReaderNET.ImageUnits.Unitless;
+            dxfDocument2.RasterVariables = rasterVariables2;
+            dxfDocument2.ThumbnailImage = null;
+            dxfDocument2.TotalEditTime = new System.DateTime(2020, 1, 31, 0, 0, 0, 0);
+            dxfDocument2.TotalEditTimeDays = 0;
+            dxfDocument2.UserTimer = new System.DateTime(2020, 1, 31, 0, 0, 0, 0);
+            dxfDocument2.UserTimerDays = 0;
+            this.dxfReaderNETControl1.DXF = dxfDocument2;
             this.dxfReaderNETControl1.FileName = null;
             this.dxfReaderNETControl1.HighlightMarkerColor2 = System.Drawing.Color.Magenta;
+            this.dxfReaderNETControl1.HighlightNotContinuous = false;
             this.dxfReaderNETControl1.Location = new System.Drawing.Point(12, 31);
             this.dxfReaderNETControl1.Name = "dxfReaderNETControl1";
             this.dxfReaderNETControl1.PlotMode = DXFReaderNET.PlotModeType.Display;
@@ -142,11 +145,33 @@
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(507, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(153, 21);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Plot preview dialog";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // printPreviewDialog1
+            // 
+            this.printPreviewDialog1.AutoScrollMargin = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.AutoScrollMinSize = new System.Drawing.Size(0, 0);
+            this.printPreviewDialog1.ClientSize = new System.Drawing.Size(400, 300);
+            this.printPreviewDialog1.Enabled = true;
+            this.printPreviewDialog1.Icon = ((System.Drawing.Icon)(resources.GetObject("printPreviewDialog1.Icon")));
+            this.printPreviewDialog1.Name = "printPreviewDialog1";
+            this.printPreviewDialog1.ShowIcon = false;
+            this.printPreviewDialog1.Visible = false;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(837, 534);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.printPreviewControl1);
@@ -155,6 +180,7 @@
             this.Controls.Add(this.dxfReaderNETControl1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "DXFReader.NET - Plot Preview Sample";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -171,6 +197,8 @@
         private System.Windows.Forms.PrintPreviewControl printPreviewControl1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.PrintPreviewDialog printPreviewDialog1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -32,18 +32,20 @@ namespace PlotPreview
 
         private void button2_Click(object sender, EventArgs e)
         {
-           
+
 
             printPreviewControl1.Visible = true;
             dxfReaderNETControl1.Visible = false;
             printPreviewControl1.Location = dxfReaderNETControl1.Location;
             printPreviewControl1.Size = dxfReaderNETControl1.Size;
-            
+
             button2.Enabled = false;
             button1.Enabled = false;
             button3.Enabled = true;
 
             dxfReaderNETControl1.PlotMode = PlotModeType.Display;
+            dxfReaderNETControl1.PlotRendering = PlotRenderingType.GrayScale;
+            dxfReaderNETControl1.PlotRendering = PlotRenderingType.GrayScale;
             dxfReaderNETControl1.PlotRendering = PlotRenderingType.GrayScale;
             dxfReaderNETControl1.PlotOffset = new Vector2(10, 10);
             dxfReaderNETControl1.PlotPreview(printPreviewControl1);
@@ -67,6 +69,26 @@ namespace PlotPreview
             {
                 dxfReaderNETControl1.Plot(printDialog.PrinterSettings.PrinterName, printDialog.PrinterSettings.Copies);
             }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            dxfReaderNETControl1.PlotRotation = PlotOrientationType.Landscape;
+            dxfReaderNETControl1.PlotRendering = PlotRenderingType.Monochrome;
+            dxfReaderNETControl1.PlotMode = PlotModeType.Extents;
+            dxfReaderNETControl1.PlotUnits = PlotUnitsType.Millimeters;
+
+
+
+            dxfReaderNETControl1.PlotPreview(printPreviewDialog1.PrintPreviewControl);
+            printPreviewDialog1.Width = this.Width;
+            printPreviewDialog1.Height = this.Height;
+            printPreviewDialog1.Text = "Plot preview dialog";
+            printPreviewDialog1.StartPosition = FormStartPosition.CenterParent;
+            printPreviewDialog1.ShowDialog();
+
+
+
         }
     }
 }
